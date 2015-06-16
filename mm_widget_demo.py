Fix compound model computation.

mm_widget_demo.py
@@ -1,34 +1,32 @@
 import sys
 
 import numpy as np
-
 import astropy.modeling.functional_models as models
 
 from sp_model_manager import SpectralModelManagerApp
 
 
-def debug_print(manager):
+def debug_print(manager, wave):
     # when GUI is killed, continue execution - here, we print components
     for component in manager.components:
         print component
 
-    # compute spectrum stored in manager
-    wave = np.arange(1,10,.1)
-    flux = manager.spectrum(wave)
-    print flux
+    # compute and print spectrum stored in manager
+    print(manager.spectrum(wave))
 
 
 def test1():
     # optional initial model is a list of component instances.
     # Note that a tie refers to other components by the 'name' attribute.
-    components = [models.Gaussian1D(2.0, 2.0, 2.0, name='test_name_1'), \
-                  models.Lorentz1D(3.0, 3.0, 3.0, tied = {'fwhm':lambda m: 1.2 * m['test_name_1'].stddev}),
-                  models.GaussianAbsorption1D(0.1, 0.1, 0.1)]
+    components = [models.Gaussian1D(1., 5., 1., name='test_name_1'), \
+                  models.Lorentz1D(0.5, 6., 0.7, tied = {'fwhm':lambda m: 1.2 * m['test_name_1'].stddev}),
+                  models.GaussianAbsorption1D(0.5, 3., 1.)]
 
     # start manager and interact with the GUI
     manager = SpectralModelManagerApp(components)
 
-    debug_print(manager)
+    wave = np.arange(1,10,.1)
+    debug_print(manager, wave)
     
 
 def test2():
@@ -39,13 +37,25 @@ def test2():
     # start manager and interact with the GUI
     manager = SpectralModelManagerApp(fname)
 
-    debug_print(manager)
+    wave = np.arange(1000.,1400.,5.)
+    debug_print(manager, wave)
 
 
 def test3():
     manager = SpectralModelManagerApp()
 
-    debug_print(manager)
+    wave = np.arange(1,10,.1)
+    debug_print(manager, wave)
+
+
+def test4():
+    import n5548_models
+    compound_model = n5548_models.model1
+
+    manager = SpectralModelManagerApp(compound_model)
+
+    wave = np.arange(1000.,1400.,5.)
+    debug_print(manager, wave)
 
 
 if __name__ == "__main__":